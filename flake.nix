ba910ab1864171b7ea8762c5916cd1c7715645ae
abhi <ugabhi@proton.me>
2024-10-29 20:26:32 +0530

    Refactoring

--- flake.nix
+++ flake.nix
@@ -13,15 +13,6 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      #
-      # Nixos module, consumed by other flakes
-      # nixosModules."otter" = { config, ... }: { options = {}; config = {}; };
-      #      nixosModules."otter" = import ./module.nix;
-      # nixosModules.default = import ./module.nix;
-      # Default module
-      # nixosModules.default = { config, ... }: { options = {}; config = {}; };
-      #
-      # homeManagerModules.otter = import ./module.nix;
       homeManagerModules.otter =
         {
           config,
@@ -85,19 +76,3 @@
       });
     };
 }
-/*
-  # otter/module.nix
-  { self, config, pkgs, lib, ... }:
-
-  {
-    options.program.otter = {
-      enable = lib.mkEnableOption "Enable the otter program.";
-      # Define any other options here if needed
-    };
-
-    config = lib.mkIf config.program.otter.enable {
-      # Define what to do when `program.otter.enable` is true
-      home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.default ];
-    };
-  }
-*/
